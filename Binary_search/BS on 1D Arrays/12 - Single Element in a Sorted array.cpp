675e855de19ef7b01d3fe930d095e15bb4f13124
aumdave1979 <aumdave1979@gmail.com>
2026-05-24 11:57:42 +0530

    12th striver sheet BS Single element

---
+++ Binary_search/BS on 1D Arrays/12 - Single Element in a Sorted array.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//if odd even pair then left side eliminate and if even odd same pair then right side of the mid will be eliminated
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int n = nums.size();
+        if(n==1) return nums[0];
+        if(nums[0] != nums[1]){
+            return nums[0];
+        }
+        if(nums[n-2]!=nums[n-1]){
+            return nums[n-1];
+        }
+  
+
+        int low = 1;
+        int high = n-2;
+
+        while(low<=high){
+            int mid = low + (high-low)/2;
+
+            if(nums[mid] != nums[mid-1] && nums[mid]!= nums[mid+1]){
+                return nums[mid];
+            }
+            //eliminating the left half
+            if((mid%2==1 && nums[mid-1]==nums[mid])  || (mid%2==0 && nums[mid] == nums[mid+1]) ){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }            
+        }
+    return -1;
+    }
+};
