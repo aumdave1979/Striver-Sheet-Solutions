75f3e7e498e1cbb3ebfea0d4f07af5e1077b39c1
aumdave1979 <aumdave1979@gmail.com>
2026-06-23 16:28:37 +0530

    optimized search in 2d matrix

--- Binary_search/BS on 2D Arrays/2 - Search in a 2D matrix.cpp
+++ Binary_search/BS on 2D Arrays/2 - Search in a 2D matrix.cpp
@@ -18,3 +18,46 @@ public:
     }
 };
 */
+
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+
+        int rows = matrix.size();
+        int cols = matrix[0].size();
+
+        int topRow = 0;
+        int bottomRow = rows - 1;
+        
+        while (topRow <= bottomRow) {
+
+            int midRow = topRow + (bottomRow - topRow) / 2;
+
+            if (target < matrix[midRow][0]) {
+                bottomRow = midRow - 1;
+            }
+            else if (target > matrix[midRow][cols - 1]) {
+                topRow = midRow + 1;
+            }
+            else {
+
+                int leftCol = 0;
+                int rightCol = cols - 1;
+                while (leftCol <= rightCol) {
+
+                    int midCol = leftCol + (rightCol - leftCol) / 2;
+                    if (matrix[midRow][midCol] == target)
+                        return true;
+
+                    if (matrix[midRow][midCol] < target)
+                        leftCol = midCol + 1;
+                    else
+                        rightCol = midCol - 1;
+                }
+                return false;
+            }
+        }
+        return false;
+    }
+};
