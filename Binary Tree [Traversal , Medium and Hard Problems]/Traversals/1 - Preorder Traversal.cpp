cfc2cd6300b4c58a8ab7b5be81540c7442836741
aumdave1979 <aumdave1979@gmail.com>
2026-06-29 12:26:11 +0530

    preorder traversal

---
+++ Binary Tree [Traversal , Medium and Hard Problems]/Traversals/1 - Preorder Traversal.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // Definition for a binary tree node.
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+  };
+ 
+class Solution {
+public:
+    vector<int> data;
+
+    void traversal(TreeNode* root){
+        if(root == nullptr){
+            return;
+        }
+        data.push_back(root->val);
+        traversal(root->left);
+        traversal(root->right);
+    }
+
+    vector<int> preorderTraversal(TreeNode* root) {
+        traversal(root);
+        return data;
+    }
+};
