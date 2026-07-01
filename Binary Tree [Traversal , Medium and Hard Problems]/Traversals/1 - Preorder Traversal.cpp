cadfc46ad351a4bd83dd26e622290df354882be7
aumdave1979 <aumdave1979@gmail.com>
2026-07-01 13:10:02 +0530

    updated preorder

--- Binary Tree [Traversal , Medium and Hard Problems]/Traversals/1 - Preorder Traversal.cpp
+++ Binary Tree [Traversal , Medium and Hard Problems]/Traversals/1 - Preorder Traversal.cpp
@@ -11,7 +11,7 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
- 
+  
 class Solution {
 public:
     vector<int> data;
