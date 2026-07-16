bf5149bcb396e32eb4d844aef6952f3a3009c80e
aumdave1979 <aumdave1979@gmail.com>
2026-07-16 16:00:18 +0530

    preorder traversal but using iterative approch using stack

--- Binary Tree [Traversal , Medium and Hard Problems]/Traversals/1 - Preorder Traversal.cpp
+++ Binary Tree [Traversal , Medium and Hard Problems]/Traversals/1 - Preorder Traversal.cpp
@@ -11,7 +11,30 @@ using namespace std;
       TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
       TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
   };
+
+  class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> preorder;
+        stack<TreeNode*> st;
+        st.push(root);
+
+        while(!st.empty()){
+            root = st.top();
+            st.pop();
+            preorder.push_back(root->val);
+            if(root->right != nullptr){
+                st.push(root->right);
+            }
+            if(root->left != nullptr){
+                st.push(root->left);
+            }
+        }
+    return preorder;
+    }
+};
   
+/*
 class Solution {
 public:
     vector<int> data;
@@ -29,4 +52,4 @@ public:
         traversal(root);
         return data;
     }
-};
+};*/
